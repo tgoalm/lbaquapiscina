140236d7a6c1888b00dfb5f9d514bf111e7b6930
Tiago Almeida <tgoalm@gmail.com>
2023-04-01 09:21:58 -0300

    Teste phpmailer

--- testemail.php
+++ testemail.php
@@ -5,6 +5,18 @@
     <title>Teste de Email</title>
 </head>
 <body>
+    <?php
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\SMTP;
+        use PHPMailer\PHPMailer\Exception;
+        require 'vendor/autoload.php';
 
+        $mail = new PHPMailer(true);
+    try {
+
+    } catch (Exception $exception) {
+
+    }
+    ?>
 </body>
 </html>
