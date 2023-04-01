e982324bae277b767904adab5e2c1dacc1534f4a
Tiago Almeida <tgoalm@gmail.com>
2023-04-01 09:00:46 -0300

    Teste phpmailer

--- bat/email.php
+++ bat/email.php
@@ -1,45 +1,86 @@
 <?php
-require 'phpmailer/PHPMailerAutoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-$configuracao = json_decode('email.config.json');
+require "PHPMailer/src/Exception.php";
+require "PHPMailer/src/PHPMailer.php";
+require "PHPMailer/src/SMTP.php";
 
-var_dump($configuracao);
-$assunto = "Contato site lbaquapiscina";
-$mensagem = "<bold>Teste<bold> envio de e-mail.";
+//require 'config.php';
 
-if (isset($_POST['assunto']) && !empty($_POST['assunto'])) {
-    $assunto = $_POST['assunto'];
+
+function send_email($toAddress, $subject, $body) {
+
+    $phpmailer = new PHPMailer(TRUE);
+    $phpmailer->isSMTP();
+    $phpmailer->Host = SMTP_Host;
+    $phpmailer->SMTPAuth = SMTP_Auth;
+    $phpmailer->Port = SMTP_Port;
+    $phpmailer->Username = SMTP_Username;
+    $phpmailer->Password = SMTP_Password;
+    $phpmailer->SMTPSecure = SMTP_SecureMode;
+
+    $phpmailer->setFrom(SMTP_SenderEmail, SMTP_SenderName);
+    $phpmailer->addReplyTo(SMTP_SenderEmail, SMTP_SenderName);
+    $phpmailer->addAddress($toAddress);
+
+    $phpmailer->Subject = $subject;
+    $phpmailer->isHTML = false;
+    $phpmailer->Body = $body;
+
+    $phpmailer->SMTPDebug = 4;
+    $phpmailer->Debugoutput = 'html';
+    $phpmailer->setLanguage('pt');
+
+    if($phpmailer->send()){
+        return 0;
+    }else{
+        $error = 'Message could not be sent. Mailer Error: ' . $phpmailer->ErrorInfo;
+        error_log($error);
+
+    }
 }
- if (isset($_POST['mensagem']) && !empty($_POST['mensagem'])) {
-    $mensagem = $_POST['mensagem'];
-}
- $mail = new PHPMailer;
 
-$mail->isSMTP();
-$mail->Host = 'mx1.hostinger.com';
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'tls';
-$mail->Username = 'contato@lbaquapiscina.com.br';
-$mail->Password = 'Gomes09@';
-$mail->Port = 587;
 
-$mail->setFrom('tialmeid@gmail.com', 'Contato');
-$mail->addAddress('lbaquapiscina@gmail.com.br');
-$mail->addAddress('contato@lbaquapiscina.com.br');
 
-$mail->isHTML(true);
-
-$mail->Subject = $assunto;
-$mail->Body    = nl2br($mensagem);
-//$mail->AltBody = nl2br(strip_tags($mensagem));
-
-if(!$mail->send()) {
-    echo 'Não foi possível enviar a mensagem.<br>';
-    echo 'Erro: ' . $mail->ErrorInfo;
-} else {
-    header('Location: index.php?enviado');
-}
-
-$mail->SMTPDebug = 4;
-$mail->Debugoutput = 'html';
-$mail->setLanguage('pt');
+//require 'phpmailer/src/PHPMailer.php';
+//require 'phpmailer/src/SMTP.php';
+//require 'phpmailer/src/Exception.php';
+//
+//$assunto = "Contato site lbaquapiscina";
+//$mensagem = "<bold>Teste<bold> envio de e-mail.";
+//
+//if (isset($_POST['assunto']) && !empty($_POST['assunto'])) {
+//    $assunto = $_POST['assunto'];
+//}
+// if (isset($_POST['mensagem']) && !empty($_POST['mensagem'])) {
+//    $mensagem = $_POST['mensagem'];
+//}
+// $mail = new PHPMailer;
+//
+//$mail->isSMTP();
+//$mail->Host = 'mx1.hostinger.com';
+//$mail->SMTPAuth = true;
+//$mail->SMTPSecure = 'tls';
+//$mail->Username = "contato@lbaquapisicna.com.br";
+//$mail->Password = 'Gomes09@';
+//$mail->Port = 587;
+//
+//$mail->setFrom('tialmeid@gmail.com', 'Contato');
+//$mail->addAddress('lbaquapiscina@gmail.com.br');
+//$mail->addAddress('contato@lbaquapiscina.com.br');
+//$mail->addAddress('tgoalm@gmail.com');
+//
+//$mail->isHTML(true);
+//
+//$mail->Subject = $assunto;
+//$mail->Body    = nl2br($mensagem);
+////$mail->AltBody = nl2br(strip_tags($mensagem));
+//
+//if(!$mail->send()) {
+//    echo 'Não foi possível enviar a mensagem.<br>';
+//    echo 'Erro: ' . $mail->ErrorInfo;
+//} else {
+//    header('Location: index.php?enviado');
+//}
+//
